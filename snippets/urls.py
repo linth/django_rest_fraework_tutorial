0ae2fe0b1c6289eb241023b3eeca9be8423d2839
gerogelin <gerogelin@oringnet.com>
2023-11-22 10:37:32 +0800

    feat: add class-based view for snippets.

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
+    # function-based view
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
+
+    # class-based view
+    path('snippets-class-based/', views.SnippetList.as_view()),
+    path('snippets-class-based/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
