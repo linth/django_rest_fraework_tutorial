1fd25957d22ec8e63cf73fbf14b8364a7aa0da77
linth <zhua0404@gmail.com>
2023-12-03 10:05:44 +0800

    feat: product's serializer, view, url.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from cart.models import Product
 
-# Create your views here.
+from cart.serializers import ProductSerializer
+
+# https://dev.to/nick_langat/building-a-shopping-cart-using-django-rest-framework-54i0
+
+
+class ProductAPI(APIView):
+    ''' Single API to handle product operations. '''
+    serializer_class = ProductSerializer
+    
+    def get(self, request, fromat=None):
+        qs = Product.objects.all()
+
+        return Response(
+            {'data': self.serializer_class(qs, many=True).data},
+            status=status.HTTP_200_OK
+        )
+        
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
--- django_rest_framework_tutorial/urls.py
+++ django_rest_framework_tutorial/urls.py
@@ -19,8 +19,12 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from user import views as user_views
+from cart.views import ProductAPI
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -44,9 +48,14 @@ urlpatterns = [
     path('users/', user_views.UserList.as_view()),
     path('users/<int:pk>/', user_views.UserDetail.as_view()),
     path('users/list/', user_views.UserView.as_view()),
+    
+    path('products/', ProductAPI.as_view(), name='products'),
 
     # for swagger.
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
